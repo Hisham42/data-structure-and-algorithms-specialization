0874995aff0a9d9bb07ba5149114b841a6efb841
Hisham <hisham.alghaish@gmail.com>
2023-12-09 07:12:08 +0200

    Complete Last Digit of Fibonacci Number

--- 01-algorithmic-toolbox/w02/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ 01-algorithmic-toolbox/w02/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -21,8 +21,69 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     return sum % 10;
 }
 
+unsigned long long sum_fibonacci_fast(unsigned long long from, unsigned long long to) {
+
+    unsigned long long sum{0};
+    if (to >= from)
+    {
+   
+    
+        if(to == 0)
+        {
+            return 0;
+        }
+        else if (to == 1) {
+            return 1;
+        }
+        else{
+            
+            to = to % 60;
+            from = from % 60;
+
+            while (to < from)
+            {
+                to += 60;
+            }
+            
+
+            unsigned long long* fArray = new unsigned long long[to + 1];
+        
+            
+            for (size_t i = 0; i <= to; i++)
+            {
+                if (i == 0)
+                {
+                    fArray[i] = 0;
+                }
+
+                else if (i == 1)
+                {
+                    fArray[i] = 1;
+                }
+                else
+                {
+                    fArray[i] = fArray[i - 1] + fArray[i - 2];
+                }   
+
+                if (i >= from)
+                {
+                    sum += fArray[i];
+                }
+                
+            }
+
+            delete[] fArray;
+            }
+    }
+
+   
+
+    return sum % 10;
+    
+}
+
 int main() {
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    std::cout << sum_fibonacci_fast(from, to) << '\n';
 }
--- 01-algorithmic-toolbox/w02/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ 01-algorithmic-toolbox/w02/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <math.h>
 int fibonacci_sum_squares_naive(long long n) {
     if (n <= 1)
         return n;
@@ -18,8 +18,85 @@ int fibonacci_sum_squares_naive(long long n) {
     return sum % 10;
 }
 
+unsigned long long fibonacci_fast(unsigned long long n) {
+
+    
+    
+    
+    if(n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else{
+         unsigned long long* fArray = new unsigned long long[n + 1];
+    
+    
+        fArray[0] = 0;
+        fArray[1] = 1;
+
+        //std::cout << fArray[0] % 10 << " " << fArray[1] % 10 << " ";
+
+        for (size_t i = 2; i < n + 1; i++)
+        {
+            fArray[i] = fArray[i - 1] + fArray[i - 2];    
+            //std::cout << fArray[i] % 10 << " ";
+        }
+
+        //std::cout << std::endl;
+        
+        unsigned long long sum = fArray[n];
+
+        delete[] fArray;
+
+        return sum;
+    }
+   
+}
+
+int fibonacci_sum_squares_magic(long long n) {
+
+    std::string cycle = "0 1 2 6 5 0 4 3 4 0 5 6 2 1 0 0 9 8 4 5 0 6 7 6 0 5 4 8 9 0";
+    int cycle_count{0};
+
+    for (auto &&i : cycle)
+    {
+        if (i !=' ')
+        {
+            cycle_count++;
+        }
+        
+    }
+    
+    n = n % cycle_count;
+    
+    return fibonacci_fast(n) % 10 * fibonacci_fast(n + 1) % 10;
+
+}
+
+
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    std::cout << fibonacci_sum_squares_magic(n);
+    // unsigned long long int sum{0};
+
+    // // for (size_t i = 0; i < 20; i++)
+    // // {
+    // //     sum += std::pow(fibonacci_fast(i), 2);
+        
+    // // }
+
+    // // std::cout << sum << " " << "\n";
+
+    // for (size_t i = 0; i < 100; i++)
+    // {
+    //     std::cout << fibonacci_sum_squares_magic(i) << " ";
+    // }
+    
+
+    
 }
