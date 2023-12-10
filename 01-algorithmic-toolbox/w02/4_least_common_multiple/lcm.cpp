9d1b495ae2d78eecfa6268e35cb904e4da45a12f
Hisham <hisham.alghaish@gmail.com>
2023-12-10 10:35:59 +0200

    Complete week 2

--- 01-algorithmic-toolbox/w02/4_least_common_multiple/lcm.cpp
+++ 01-algorithmic-toolbox/w02/4_least_common_multiple/lcm.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+int gcd_fast(int a, int b) {
+  if (b % a == 0)
+  {
+    return a;
+  }
+  
+  else
+  {
+    int c = b % a;
+    return gcd_fast(c, a);  
+  }
+}
+
 long long lcm_naive(int a, int b) {
   for (long l = 1; l <= (long long) a * b; ++l)
     if (l % a == 0 && l % b == 0)
@@ -8,9 +21,16 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+long long lcm_fast(long long a, long long b) {
+
+  return (a*b / gcd_fast(a,b));
+
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
+
   return 0;
 }
