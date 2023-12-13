e2b42bdbd6b4be8ef7dbf035ab50a9d154d131f4
Hisham <hisham.alghaish@gmail.com>
2023-12-13 03:41:34 +0200

    Complete Max Ads revenue and Collecting Signatures

--- 01-algorithmic-toolbox/w03/4_maximum_advertisement_revenue/dot_product.cpp
+++ 01-algorithmic-toolbox/w03/4_maximum_advertisement_revenue/dot_product.cpp
@@ -1,16 +1,53 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using std::vector;
 
 long long max_dot_product(vector<int> a, vector<int> b) {
   // write your code here
-  long long result = 0;
+  double result = 0;
   for (size_t i = 0; i < a.size(); i++) {
-    result += ((long long) a[i]) * b[i];
+    
+    // calculate price index
+    int price_index{0};
+    double max_price{0};
+
+    int click_index{0};
+    int max_click{0};
+
+    for (size_t j = 0; j < a.size(); j++)
+    {
+      if (max_price < a[j])
+      {
+        max_price = a[j];
+        price_index = j;
+      }
+
+      if (max_click < b[j])
+      {
+        max_click = b[j];
+        click_index = j;
+      }
+      
+    }
+    
+    result += max_click * max_price;
+
+    a[price_index] = 0;
+    b[click_index] = 0;
+
   }
+
   return result;
+}
+
+void testSolution() {
+
+  assert(max_dot_product({23}, {39}) == 897);
+  assert(max_dot_product({2, 3, 9}, {7, 4, 2}) == 79);
+
 }
 
 int main() {
@@ -24,4 +61,5 @@ int main() {
     std::cin >> b[i];
   }
   std::cout << max_dot_product(a, b) << std::endl;
+  // testSolution();
 }
--- 01-algorithmic-toolbox/w03/5_collecting_signatures/covering_segments.cpp
+++ 01-algorithmic-toolbox/w03/5_collecting_signatures/covering_segments.cpp
@@ -7,17 +7,45 @@ using std::vector;
 
 struct Segment {
   int start, end;
+  bool touched{false};
 };
 
-vector<int> optimal_points(vector<Segment> &segments) {
-  vector<int> points;
-  //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+bool apply(int location, vector<Segment>& segments) {
+  bool a = false;
+
+  for (int i = 0; i < segments.size(); i++)
+  {
+    if (segments[i].touched == false && location >= segments[i].start && location <= segments[i].end)
+    {
+      segments[i].touched = true;
+      a = true;
+    }
+    
   }
-  return points;
+  return a;
 }
+
+vector<int> new_solution(vector<Segment> &segments) {
+  vector<int> solution;
+
+  std::sort(segments.begin(), segments.end(), [](const Segment& a, const Segment& b) {
+    return (a.end) < (b.end);
+  });
+
+  for (size_t i = 0; i < segments.size(); i++)
+  {
+    if (segments[i].touched == false && apply(segments[i].end, segments))
+    {
+      solution.push_back(segments[i].end);
+    }
+    
+  }
+  
+  return solution;
+}
+
+
+
 
 int main() {
   int n;
@@ -26,7 +54,7 @@ int main() {
   for (size_t i = 0; i < segments.size(); ++i) {
     std::cin >> segments[i].start >> segments[i].end;
   }
-  vector<int> points = optimal_points(segments);
+  vector<int> points = new_solution(segments);
   std::cout << points.size() << "\n";
   for (size_t i = 0; i < points.size(); ++i) {
     std::cout << points[i] << " ";
