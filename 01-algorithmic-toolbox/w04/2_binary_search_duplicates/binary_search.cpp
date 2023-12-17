6ff75e531bc170d912ebde2efeae0c3240690b2f
Hisham <hisham.alghaish@gmail.com>
2023-12-17 15:14:17 +0200

    Complete Majority Element

--- 01-algorithmic-toolbox/w04/2_binary_search_duplicates/binary_search.cpp
+++ 01-algorithmic-toolbox/w04/2_binary_search_duplicates/binary_search.cpp
@@ -4,9 +4,29 @@
 
 using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
+int binary_search(const vector<int> &a, int x, int left, int right) {
+  
+  int middle = (right + left) / 2;
+  // base case 
+  if (right <= left && x != a[right])
+  {
+    return -1;
+  }
+  
+  if (x == a[middle] && a[middle - 1] != x)
+  {
+    return middle;
+  }
+  else if (x > a[middle])
+  {
+    return binary_search(a, x, middle + 1, right);
+  }
+  else if (x < a[middle] || a[middle - 1] == x)
+  {
+    return binary_search(a, x, left, middle - 1);
+  }
+  else
+    return -1;
 }
 
 int linear_search(const vector<int> &a, int x) {
@@ -14,6 +34,10 @@ int linear_search(const vector<int> &a, int x) {
     if (a[i] == x) return i;
   }
   return -1;
+}
+
+void test_solution() {
+  assert(binary_search({2, 4, 4, 4, 7, 7, 9}, 4, 0, 6) == 1);
 }
 
 int main() {
@@ -31,6 +55,9 @@ int main() {
   }
   for (int i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
+    std::cout << binary_search(a, b[i], 0 , a.size() - 1) << ' ';
   }
+
+  // test_solution();
 }
+
--- 01-algorithmic-toolbox/w04/3_majority_element/majority_element.cpp
+++ 01-algorithmic-toolbox/w04/3_majority_element/majority_element.cpp
@@ -1,14 +1,124 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using std::vector;
+
+vector<int> merge_sort(vector<int>& a, int left, int right) {
+
+
+  int mid = (right - left) / 2;
+  
+  if (right > left)
+  {
+    vector<int> merged;
+    vector<int> l;
+    vector<int> r;
+
+    
+      for (size_t i = 0; i <= mid; i++)
+      {
+        l.push_back(a[i]);
+      }
+
+      for (size_t j = mid + 1; j < a.size(); j++)
+      {
+        r.push_back(a[j]);
+        
+      }
+
+      l = merge_sort(l, 0, mid);
+      r = merge_sort(r, mid + 1, a.size() - 1);
+      
+
+      // merging
+      int q{0}, w{0};
+      for (; merged.size() < l.size() + r.size();)
+      {
+        if (q == l.size() && w < r.size())
+        {
+          merged.push_back(r[w]);
+          w++;
+        }
+
+        else if (w == r.size() && q < l.size())
+        {
+          merged.push_back(l[q]);
+          q++;
+        }
+
+        else if (q < l.size() && w < r.size())
+        {
+          if (l[q] >= r[w])
+          {
+            merged.push_back(l[q]);
+            q++;
+          }
+          else
+          {
+            merged.push_back(r[w]);
+            w++;
+          }
+          
+        }
+
+      }
+    return merged;
+  }
+  else
+    return a;
+    
+}
+
 
 int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
   //write your code here
+
+  // std::sort(a.begin(), a.end(), [](int a, int b) {
+  //   return a < b;
+  // });
+
+  a = merge_sort(a, 0, a.size() - 1);
+
+  int count{1};
+
+  for (size_t i = 0; i < a.size(); i++)
+  {
+    if (count > (a.size() / 2))
+    {
+      return 1;
+    }
+    
+    if (a[i] == a[i+1])
+    {
+      count++;
+    }
+    else
+      count = 1;
+  }
+  
   return -1;
+}
+
+void test_solution() {
+  vector<int> a = {2, 3, 9, 2, 2};
+  assert(get_majority_element(a, 0, 4) == 1);
+
+  vector<int> b = {1, 2, 3, 1};
+  assert(get_majority_element(b, 0, 3) == -1);
+}
+
+void test_solution2() {
+  vector<int> a = {2, 3, 9, 2, 2, 10, 20, 45, 74};
+  vector<int> a_sorted = merge_sort(a, 0, a.size() - 1);
+  for (size_t i = 0; i < a.size(); i++)
+  {
+    std::cout << a_sorted[i] << " ";
+  }
+  
 }
 
 int main() {
@@ -18,5 +128,6 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  std::cout << (get_majority_element(a, 0, a.size() - 1) != -1) << '\n';
+  // test_solution();
 }
