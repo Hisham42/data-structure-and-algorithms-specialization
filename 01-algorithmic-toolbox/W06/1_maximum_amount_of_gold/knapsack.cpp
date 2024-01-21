84c4295df23f3dd572406d87f91a824a57f0b2a8
Hisham Alghaish <103466249+Hisham42@users.noreply.github.com>
2024-01-21 03:29:36 +0200

    Complete Knapsack dp

--- 01-algorithmic-toolbox/W06/1_maximum_amount_of_gold/knapsack.cpp
+++ 01-algorithmic-toolbox/W06/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <iomanip>
 
 using std::vector;
 
@@ -14,6 +16,68 @@ int optimal_weight(int W, const vector<int> &w) {
   return current_weight;
 }
 
+
+int knapsack(int W, vector<int> &w) {
+
+  std::sort(w.begin(), w.end(), [](int a, int b) {
+    return a < b;
+  });
+
+  vector<vector<int>> backPack (w.size(), vector<int>(W + 1));
+
+  for (size_t i = 0; i < w.size(); i++)
+  {
+    backPack[i][0] = 0;
+  }
+
+  for (size_t i = 0; i < w.size(); i++)
+  {
+    int a{-1}, b{-1};
+    for (size_t j = 1; j < W + 1; j++)
+    {
+      if (w[i] <= j && i == 0)
+      {
+        backPack[i][j] = w[i];
+      }
+      else if (w[i] <= j)
+      {
+
+        if (i >= 1)
+        {
+          if (backPack[i - 1][j] <= j)
+          {
+            a = backPack[i - 1][j];
+          }
+        }
+   
+        if (i >= 1 && j - w[i] >= 0)
+        {
+          if (backPack[i - 1][j - w[i]] + w[i] <= j)
+          {
+            b = backPack[i - 1][j - w[i]] + w[i];
+          }
+        }
+        backPack[i][j] = std::max({a, b});
+      }
+      else if (i >= 1)
+      {
+        backPack[i][j] = backPack[i - 1][j];
+      }
+    }
+  }
+
+  // for (size_t i = 0; i < w.size(); i++)
+  // {
+  //   for (size_t j = 0; j < W + 1; j++)
+  //   {
+  //     std::cout << std::setw(3) << backPack[i][j];
+  //   }
+  //   std::cout << std::endl;
+  // }
+
+  return backPack[w.size() - 1][W];
+}
+
 int main() {
   int n, W;
   std::cin >> W >> n;
@@ -21,5 +85,5 @@ int main() {
   for (int i = 0; i < n; i++) {
     std::cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  std::cout << knapsack(W, w) << '\n';
 }
